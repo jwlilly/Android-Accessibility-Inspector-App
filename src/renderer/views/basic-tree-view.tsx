fafa234408a90710601ee72398079471d10d4f50
John Lilly <lilly.john.w@gmail.com>
2024-09-17 14:01:54 -0400

    WIP - Styling updates

--- src/renderer/views/basic-tree-view.tsx
+++ src/renderer/views/basic-tree-view.tsx
@@ -118,14 +118,14 @@ const BasicTreeView = function BasicTreeView({
             )}
             <div className="indicator">
               <span
-                className={`justify-start max-w-60 text-start btn ${isSelected ? 'btn-outline btn-secondary' : 'btn-ghost'}`}
+                className={`justify-start max-w-60 text-start btn ${isSelected ? 'btn-secondary' : 'btn-neutral btn-outline'}`}
               >
                 <div className="flex flex-row overflow-hidden whitespace-nowrap">
                   <div className="flex-col">
                     {element.name}
                     {element.metadata !== undefined &&
                     element.metadata.text !== undefined ? (
-                      <div className="overflow-hidden text-xs font-thin text-ellipsis whitespace-nowrap">
+                      <div className="overflow-hidden max-w-44 text-xs font-thin text-ellipsis whitespace-nowrap">
                         {element.metadata.text}
                       </div>
                     ) : null}
